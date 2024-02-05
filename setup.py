2d061f10e8603d1e7030f5de66ec835045ba13f7
Pseudoboertjie <148277399+Pseudoboertjie@users.noreply.github.com>
2024-02-05 17:04:30 +0200

    Just a Test

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+import os
+import urllib.request
 
+def download_and_install_vscode():
+    installer_url = "https://go.microsoft.com/fwlink/?LinkID=623548"
+    installer_filename = "vscode.exe"
 
-print("hello")
+    # Download the installer
+    urllib.request.urlretrieve(installer_url, installer_filename)
+
+    # Run the installer
+    os.startfile(installer_filename)
+
+if __name__ == "__main__":
+    download_and_install_vscode()
+
